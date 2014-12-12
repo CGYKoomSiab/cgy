30405b52b78ee5f1eda92e5c0b0960a49ff236df
Chue Yang <yangchuenou@gmail.com>
2014-12-11 23:45:05 -0600

    Changes for index testing

--- cgy/src/cgy/helloworld/HelloWorld.java
+++ cgy/src/cgy/helloworld/HelloWorld.java
@@ -15,6 +15,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println("am i doing this right?");
 	}
 
 }
--- cgy/web/index.jsp
+++ cgy/web/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+ i made some changes
 </body>
 </html>
