0b7809754b6377a370d030b4139fb4e026f373f3
Chue Yang <yangchuenou@gmail.com>
2014-12-11 23:41:47 -0600

    Initial commit

---
+++ cgy/src/cgy/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cgy.helloworld;
+
+/**
+ * @author chueyang
+ * @date Dec 11, 2014
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
