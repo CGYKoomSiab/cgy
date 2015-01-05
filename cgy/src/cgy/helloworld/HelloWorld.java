9e531fe547ba47039a64c7f5e2093b79aa212b63
Chue Yang <yangchuenou@gmail.com>
2015-01-05 10:35:28 -0600

    added cgy files to project

--- cgy/src/cgy/helloworld/HelloWorld.java
+++ cgy/src/cgy/helloworld/HelloWorld.java
@@ -3,6 +3,8 @@
  */
 package cgy.helloworld;
 
+import java.util.Scanner;
+
 /**
  * @author chueyang
  * @date Dec 11, 2014
@@ -14,9 +16,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
 		
-		System.out.println("am i doing this right?");
 	}
 
 }
